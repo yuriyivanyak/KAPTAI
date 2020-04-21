5023bae3aa498522e1e453d9c6be55450bfb713e
yuriyivanyak <federallacey21@gmail.com>
2020-04-22 01:26:20 +0200

    Add Carousel

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 15,
+        nav:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                autoplay:true,
+                autoplayTimeout:6000,
+                autoplayHoverPause:true
+            },
+            600:{
+                items:1,
+                margin:0,
+                nav:true
+            },
+            1000:{
+                items:2,
+                nav:true,
+            }
+        }
+    })
+});
